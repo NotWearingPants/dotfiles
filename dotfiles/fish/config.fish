8c4ded34b71382ce9246689788624f47953866ee
Snir Broshi <snir@broshi.net>
2019-08-24 17:45:20 +0300

    added my fish config

---
+++ dotfiles/fish/config.fish
@@ -0,0 +1,13 @@
+# add directories to PATH
+# TODO: get PATH directories somehow
+#set --export PATH ... $PATH
+
+# aliases for `ls`
+alias l='ls -lah'
+alias la='ls -lAh'
+alias ll='ls -lh'
+alias ls='ls -G'
+alias lsa='ls -lah'
+
+# for x server programs
+set --export DISPLAY :0
