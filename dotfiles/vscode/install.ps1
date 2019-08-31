8afad1c3a3df448ebe3fac62ada9083a4c408113
Snir Broshi <snir@broshi.net>
2019-08-31 13:44:36 +0300

    vscode extension installer now shows extensions already installed separately

--- dotfiles/vscode/install.ps1
+++ dotfiles/vscode/install.ps1
@@ -1,14 +1,28 @@
 function Install-VSCodeExtensions {
-    $extensions = $args[0]
+    $extensionsToInstall = $args[0]
 
-    "About to install {0} extensions..." -f $extensions.length
+    "About to install {0} extensions..." -f $extensionsToInstall.length
 
     start -Wait -NoNewWindow -FilePath "code" -ArgumentList (@("--force") + (
-        $extensions | foreach { "--install-extension", $_ }
+        $extensionsToInstall | foreach { "--install-extension", $_ }
     ))
 
     "Finished installing all extensions."
 }
 
-$extensions = sls '^[^#]' $PSScriptRoot\extensions.txt | select -expand line
-Install-VSCodeExtensions $extensions
+function Get-VSCodeExtensions {
+    $installedExtensions = (code --list-extensions)
+
+    return $installedExtensions
+}
+
+
+$extensionsToInstall = sls '^[^#]' $PSScriptRoot\extensions.txt | select -expand line
+
+$installedExtensions = Get-VSCodeExtensions
+$additionalExtensions = $installedExtensions | where { $extensionsToInstall -notcontains $_ }
+
+"Extensions installed separately:"
+$additionalExtensions | foreach { "`t" + $_ }
+
+Install-VSCodeExtensions $extensionsToInstall
