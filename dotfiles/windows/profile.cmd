60ae1acf7bb7ca886abf2c5b8c62c16bf52bd2ce
Snir Broshi <snir@broshi.net>
2019-08-31 18:19:10 +0300

    add two cmd macros - disable bash, and exit with ^D

--- dotfiles/windows/profile.cmd
+++ dotfiles/windows/profile.cmd
@@ -1,3 +1,11 @@
 @echo off
 
+:: disable `bash` to force using `wsl` (to open the default shell instead)
+:: TODO: this only disables `bash` through cmd, not powershell or Win+R
+doskey bash=echo No, run `wsl`
+
+:: exit with ^D and Enter
+doskey =exit
+
+:: greetings
 echo Welcome!
