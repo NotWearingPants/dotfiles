a383eff28be21b76d3c7706cfb6072fb29e67bc5
Snir Broshi <snir@broshi.net>
2019-08-31 15:38:30 +0300

    added enabling telnet & WSL in windows install script

--- dotfiles/windows/install.ps1
+++ dotfiles/windows/install.ps1
@@ -1,1 +1,10 @@
+#requires -RunAsAdministrator
+
+# change settings in registry
 regedit $PSScriptRoot\setup.reg
+
+# enable the telnet command
+Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient
+
+# enable WSL
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
