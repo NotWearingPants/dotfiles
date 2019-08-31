53e52df3ba907051c0b36cfe0e749541761a7f7c
Snir Broshi <snir@broshi.net>
2019-08-31 18:09:34 +0300

    added a cmd startup script

--- dotfiles/windows/install.ps1
+++ dotfiles/windows/install.ps1
@@ -8,3 +8,6 @@ Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient
 
 # enable WSL
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+# create a link to the `profile.cmd` file that runs when cmd starts
+New-Item -ItemType SymbolicLink "$env:USERPROFILE\profile.cmd" -Target "$PSScriptRoot\profile.cmd"
