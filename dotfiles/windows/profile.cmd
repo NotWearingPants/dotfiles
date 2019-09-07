8ca698f659ea3b2524066afa04c9b8989a44ba29
Snir Broshi <snir@broshi.net>
2019-09-07 15:11:01 +0300

    customize cmd prompt

--- dotfiles/windows/profile.cmd
+++ dotfiles/windows/profile.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+:: prompt
+prompt [$T$H$H$H$H$H$H]$S$P$G$S
+
 :: disable `bash` to force using `wsl` (to open the default shell instead)
 :: TODO: this only disables `bash` through cmd, not powershell or Win+R
 doskey bash=echo No, run `wsl`
