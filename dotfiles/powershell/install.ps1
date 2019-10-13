8cc91624360b9553456d2fb0244d1dbb95941683
Snir Broshi <snir@broshi.net>
2019-10-14 01:46:27 +0300

    deleted weird invisible characters from powershell's install.ps1 (soft-hyphen (U+00AD) between "Windows" and "PowerShell")

--- dotfiles/powershell/install.ps1
+++ dotfiles/powershell/install.ps1
@@ -1,2 +1,2 @@
-New-Item -ItemType Directory "$env:USERPROFILE\Documents\Windows­PowerShell"
-New-Item -ItemType SymbolicLink "$env:USERPROFILE\Documents\Windows­PowerShell\profile.ps1" -Target "$PSScriptRoot\profile.ps1"
+New-Item -ItemType Directory "$env:USERPROFILE\Documents\WindowsPowerShell"
+New-Item -ItemType SymbolicLink "$env:USERPROFILE\Documents\WindowsPowerShell\profile.ps1" -Target "$PSScriptRoot\profile.ps1"
