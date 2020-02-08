ddfb325413ddc2fded05fe349d65b427fd1ac0ba
Snir Broshi <snir@broshi.net>
2020-02-08 22:30:54 +0200

    added todos in windows

--- dotfiles/windows/install.ps1
+++ dotfiles/windows/install.ps1
@@ -4,10 +4,13 @@
 regedit $PSScriptRoot\setup.reg
 
 # enable the telnet command
+# TODO: check if the feature is already enabled, and skip this
 Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient
 
 # enable WSL
+# TODO: check if the feature is already enabled, and skip this
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 # create a link to the `profile.cmd` file that runs when cmd starts
+# TODO: check if it already exists - fail if it exists and isn't our link, but pass if it is
 New-Item -ItemType SymbolicLink "$env:USERPROFILE\profile.cmd" -Target "$PSScriptRoot\profile.cmd"
