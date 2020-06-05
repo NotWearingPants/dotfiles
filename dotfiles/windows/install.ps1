e3927e020791c051dcb7254c24671620d4d3eee0
Snir Broshi <snir@broshi.net>
2020-06-05 17:54:24 +0300

    added some todos to windows install

--- dotfiles/windows/install.ps1
+++ dotfiles/windows/install.ps1
@@ -6,10 +6,14 @@ regedit $PSScriptRoot\setup.reg
 # enable the telnet command
 # TODO: check if the feature is already enabled, and skip this
 Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient
+# TODO: restart if needed
 
 # enable WSL
 # TODO: check if the feature is already enabled, and skip this
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+# TODO: restart if needed
+
+# TODO: install Ubuntu (WSL) and Windows Terminal from the Microsoft Store somehow
 
 # create a link to the `profile.cmd` file that runs when cmd starts
 # TODO: check if it already exists - fail if it exists and isn't our link, but pass if it is
