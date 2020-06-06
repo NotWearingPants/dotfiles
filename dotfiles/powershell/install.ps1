76584f1c56abf004ef636f22280640a4b104032b
Snir Broshi <snir@broshi.net>
2020-06-06 23:49:09 +0300

    powershell install script requires admin for symlink

--- dotfiles/powershell/install.ps1
+++ dotfiles/powershell/install.ps1
@@ -1,2 +1,7 @@
+#requires -RunAsAdministrator
+
+# create a link documents to the profile file
+# TODO: check if it already exists - fail if it exists and isn't our link, but pass if it is
+# rm "~\Documents\WindowsPowerShell\profile.ps1"
 New-Item -ItemType Directory "~\Documents\WindowsPowerShell"
 New-Item -ItemType SymbolicLink "~\Documents\WindowsPowerShell\profile.ps1" -Target "$PSScriptRoot\profile.ps1"
