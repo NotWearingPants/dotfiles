303197f2541de164290522691e635b75d7cce46c
Snir Broshi <snir@broshi.net>
2020-07-24 22:35:55 +0300

    [powershell] save the last result to $_

--- dotfiles/powershell/profile.ps1
+++ dotfiles/powershell/profile.ps1
@@ -2,3 +2,14 @@
 # "Welcome"
 
 New-Alias which Get-Command
+
+# save the last result to $_
+function Out-Default {
+	# NOTE: $input is some sort of an enumerator, so we have to pipe it to get the result
+	$input | Tee-Object -Variable result | Microsoft.PowerShell.Core\Out-Default
+
+	# save the result into the $_ variable, but only if it's not an error
+	if ($result -isNot [System.Management.Automation.ErrorRecord]) {
+		$global:_ = $result
+	}
+}
