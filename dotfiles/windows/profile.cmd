67be3025cfa2eff6b57861467506b3e082e08323
Snir Broshi <snir@broshi.net>
2020-07-25 00:37:24 +0300

    [windows] alias ls to dir in cmd

--- dotfiles/windows/profile.cmd
+++ dotfiles/windows/profile.cmd
@@ -7,6 +7,9 @@ doskey bash=echo No, run `wsl`
 :: exit with ^D and Enter
 doskey =exit
 
+:: ls alias
+doskey ls=dir
+
 :: greetings
 :: TODO: this also outputs in non-login shells, make it stop
 REM echo Welcome!
