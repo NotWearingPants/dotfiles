29d4206b687e55a60e426b378d60264ded872a30
Snir Broshi <snir@broshi.net>
2020-07-25 17:05:49 +0300

    [powershell] support powershell core

--- dotfiles/powershell/install.ps1
+++ dotfiles/powershell/install.ps1
@@ -2,9 +2,13 @@
 
 Import-Module "$PSScriptRoot\..\..\scripts\utils.psm1"
 
-# create a link documents to the profile file
+# create a link in documents to the profile file for Windows PowerShell
 New-Folder '~\Documents\WindowsPowerShell'
 New-Symlink '~\Documents\WindowsPowerShell\profile.ps1' "$PSScriptRoot\profile.ps1"
+
+# create a link in documents to the profile file for Powershell Core
+New-Folder '~\Documents\PowerShell'
+New-Symlink '~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1' "$PSScriptRoot\profile.ps1"
 
 # download offline help
 Update-Help
