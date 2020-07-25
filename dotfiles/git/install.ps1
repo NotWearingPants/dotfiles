b56df9e451f6da4dd0a0e210d66104a666ae40d0
Snir Broshi <snir@broshi.net>
2020-07-25 03:50:32 +0300

    [git] typo

--- dotfiles/git/install.ps1
+++ dotfiles/git/install.ps1
@@ -2,7 +2,7 @@
 
 Import-Module "$PSScriptRoot\..\..\scripts\utils.psm1"
 
-# create link in home to the global gitignore and gitconfig files
+# create links in home to the global gitignore and gitconfig files
 New-Folder '~\.config\git\'
 New-Symlink '~\.config\git\ignore' "$PSScriptRoot\_.gitignore"
 New-Symlink '~\.config\git\config' "$PSScriptRoot\_.gitconfig"
