4a7e07d7a6f1476f8462cb2ffda04eb7a3d99bff
Snir Broshi <snir@broshi.net>
2021-06-30 16:24:00 +0300

    [python] added some more imports

--- dotfiles/python/_.pythonrc.py
+++ dotfiles/python/_.pythonrc.py
@@ -1,6 +1,8 @@
 import base64
 import collections
 import datetime
+import enum
+import hashlib
 import io
 import itertools
 import json
@@ -15,11 +17,13 @@ import subprocess
 import sys
 import threading
 import time
+from functools import reduce
 from pathlib import Path
 from pprint import pprint
 from timeit import timeit
 
 # NOTE: these are disabled because they take too much time to import
+# import PIL.Image
 # import numpy as np
 # import pandas as pd
-# import PIL.Image
+# import matplotlib.pyplot as plt
