b87292ec94e0f14332ed738497badf884652fc46
NotWearingPants <26556598+NotWearingPants@users.noreply.github.com>
2024-05-24 02:55:16 +0300

    [pug-lint] call install script from npm install script

--- dotfiles/npm/install.ps1
+++ dotfiles/npm/install.ps1
@@ -10,11 +10,14 @@ function Install-NPMPackages {
     'Finished installing all packages.'    
 }
 
+# TODO: symlink `.npmrc`
+
 $packagesToInstall = Load-ListFile "$PSScriptRoot\packages.txt"
 Install-NPMPackages $packagesToInstall
 
 # setup packages that need more setup
 . "$PSScriptRoot\..\eslint\install.ps1"
 . "$PSScriptRoot\..\stylelint\install.ps1"
+. "$PSScriptRoot\..\pug-lint\install.ps1"
 
 Remove-Module utils
