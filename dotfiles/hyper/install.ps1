364f3b13b653e2fd297699b521093e501bb31132
NotWearingPants <26556598+NotWearingPants@users.noreply.github.com>
2024-05-24 15:14:46 +0300

    normalize appdata usage

--- dotfiles/hyper/install.ps1
+++ dotfiles/hyper/install.ps1
@@ -2,7 +2,7 @@
 
 Import-Module "$PSScriptRoot\..\..\scripts\utils.psm1"
 
-# create a link in app-data to the `_.hyper.js` config file
+# create a link in AppData to the `_.hyper.js` config file
 New-Symlink "$env:APPDATA\Hyper\.hyper.js" "$PSScriptRoot\_.hyper.js"
 
 Remove-Module utils
--- dotfiles/vim/install.ps1
+++ dotfiles/vim/install.ps1
@@ -2,7 +2,7 @@
 
 Import-Module "$PSScriptRoot\..\..\scripts\utils.psm1"
 
-# create a link in app data to the vimrc file
-New-Symlink '~\AppData\Local\nvim\init.vim' "$PSScriptRoot\_2.vimrc"
+# create a link in AppData to the vimrc file
+New-Symlink "$env:LOCALAPPDATA\nvim\init.vim" "$PSScriptRoot\_2.vimrc"
 
 Remove-Module utils
